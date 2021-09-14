b65e7617361ac2079286a1fe517b3091ebd9f230
trossossp3 <21trossos@gmail.com>
2021-09-14 14:34:02 -0400

    lab one

---
+++ hello.py
@@ -0,0 +1,15 @@
+name = "trent"
+print("hello",name, sep=", ")
+
+hermione = "Hermione Granger"
+harry = "Harry Potter"
+print("Hello, "+hermione+" and "+harry+". Your names are "+harry+" and "+hermione+". Hi there. Your names are still "+hermione+" and "+harry+".")
+# Hello, "+hermione+" and "+harry+". Your names are "+harry+" and "+hermione+". Hi there. Your names are still "+hermione+" and "+harry+".
+
+greetee = "vomorte"
+
+if(greetee == "voldemorte"):
+    print("I'm not talking to you.")
+else:
+    print("hello "+greetee)
+
