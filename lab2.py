babec88a2dbb1b7109b5b34984aadf59ff20c37e
trossossp3 <21trossos@gmail.com>
2021-09-20 12:33:39 -0400

    lab 2

---
+++ lab2.py
@@ -0,0 +1,67 @@
+def display_current_value():
+    print("Current value:", current_value)
+
+
+def add(to_add):
+    global current_value, prev_value
+    prev_value = current_value
+    current_value += to_add
+
+
+def mutliply(to_multiply):
+    global current_value, prev_value
+    prev_value = current_value
+    current_value *= to_multiply
+
+
+def divide(to_divide):
+    if(to_divide == 0):
+        print("cant divide by 0")
+        return
+    global current_value, prev_value
+    prev_value = current_value
+    current_value /= to_divide
+
+
+def save():
+    global temp
+    temp = current_value
+    print("value: " + str(temp) + " saved to memory")
+
+
+def recall():
+    global current_value, temp
+    current_value = temp
+    print("saved value: "+str(temp)+" set to current value")
+
+def undo():
+    global current_value, prev_value
+    current_value, prev_value = prev_value, current_value
+
+if __name__ == "__main__":
+    current_value = 0
+    prev_value =0
+    display_current_value()
+    add(5)
+    display_current_value()
+    undo()
+    display_current_value()
+    mutliply(2)
+    display_current_value()
+    divide(12)
+    display_current_value()
+    save()
+    add(12)
+    display_current_value()
+    recall()
+    display_current_value()
+    add(5)
+    save()
+    display_current_value()
+    undo()
+    display_current_value()
+    undo()
+    display_current_value()
+  
+
+
