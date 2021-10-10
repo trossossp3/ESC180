dcc000d5b82fc44102687b7b3a3d4fe5836be2de
trossossp3 <21trossos@gmail.com>
2021-10-09 22:48:35 -0400

    worked on porject1

--- gamify.py
+++ gamify.py
@@ -1,6 +1,7 @@
 from re import match
 from typing import Match
-#tests
+# tests
+
 
 def initialize():
     '''Initializes the global variables needed for the simulation.
@@ -16,8 +17,12 @@ def initialize():
     global star_offer_time
     global cur_star_activity
     global tired
+    global time_since_activity
+    global curr_run_duration
+    time_since_activity = 120
     cur_hedons = 0
     cur_health = 0
+    curr_run_duration = 0
     tired = None
     cur_star = None
     star_offer_time = None
@@ -33,114 +38,154 @@ def initialize():
     last_finished = -1000
 
 
-def star_can_be_taken(activity):
-    pass
-
-
-
 def perform_activity(activity, duration):
-    star_elegible = get_star_elegible(activity)
+    global cur_hedons, cur_health, tired, cur_time, time_since_activity, last_activity, last_activity_duration, curr_run_duration
+    star_elegible = int(star_can_be_taken(activity))
+    tired = is_tired()
+    if(activity == "running"):
+        do_run(duration, star_elegible)
+    elif(activity == "textbooks"):
+        do_textbook(duration, star_elegible)
+    elif(activity == "resting"):
+        time_since_activity += duration
+    else:
+        return
+    cur_time += duration
+    
+def do_textbook(duration, star_elegible):
+    global cur_hedons, cur_health, tired, cur_time, time_since_activity, last_activity, last_activity_duration, curr_run_duration
+    curr_run_duration=0
+    cur_health += 2*duration
 
-    if(star_elegible):
-            if(activity == "running"):
-                if not tired:
-                    if duration < 10:
-                        cur_hedons += (2+3)*10
-                        cur_health += 3*duration
-                    elif duration > 10 and duration < 180:
-                        cur_hedons += (2+3)*10 + -2*(duration-10)
-                        cur_health += 3*duration
-                    else:
-                        cur_hedons += (2+3)*10 + -2*(duration-10)
-                        cur_health += 3*180 + 1*(duration-180)
-                else:
-                    if duration < 10:
-                        cur_hedons += (2+3)*10
-                        cur_health += duration*-2
-                    elif duration > 10 and duration < 180:
-                        cur_hedons += (2+3)*10 + -2*(duration-10)
-                        cur_health += duration*-2
-                    else:
-                        cur_hedons += (2+3)*10 + -2*(duration-10)
-                        cur_health += duration*-2
-            elif(activity == "textbooks"):
-                cur_health+=2*duration
-                if not tired:
-                    if duration <=20:
-                        cur_hedons+=duration
-                    else:
-                        cur_hedons+=duration*-1
-                else:
-                    cur_hedons+=-2*duration
-            else:
-                return 
+    if not tired:  # not tired if not tired or using star
+        if duration <= 20:
+                # if activity for 10 or under apply star bonus for that time if walked more than 10 only do start bonus for 10
+            cur_hedons += duration + \
+            (3*duration*star_elegible if duration <11 else 3*10*star_elegible)
+        else:
+                # star elegible will be either the star bonus or 0
+            cur_hedons += 10 + (duration-10)*-1 + (3*10*star_elegible)
+
+    else:
+        curr_run_duration=0
+        cur_hedons += -2*duration
+        time_since_activity = 0
+
+def do_run(duration, star_elegible):
+    global tired, cur_hedons, cur_health, curr_run_duration, time_since_activity
+    if not tired:  # not tired if not tired or using star
+        if duration < 10:
+            cur_hedons += 2*10 + (3*star_elegible*10)
+            cur_health += 3*duration
+        elif duration > 10 and duration < 180:
+            cur_hedons += (2)*10+(3*star_elegible *
+                                    10) + -2*(duration-10)
+            cur_health += 3*duration
+        else:
+            cur_hedons += (2)*10+(3*star_elegible *10) + -2*(duration-10)
+            cur_health += 3*180 + 1*(duration-180)
+    else:
+
+        if duration+curr_run_duration < 10:
+            cur_hedons += (2)*10+(3*star_elegible*10)
+            cur_health += 3*duration
+
+        elif duration+curr_run_duration > 10 and duration+curr_run_duration < 180:
+            cur_hedons += (-2)*10+(3*star_elegible * 10) + -2*(duration-10)
+            cur_health += 3*duration
+
+        else:
+            cur_hedons += (-2)*10+(3*star_elegible * 10) + -2*(duration-10)
+            cur_health+=((duration+curr_run_duration)-180 * 1) + (duration-(duration+curr_run_duration-180))*3
+    time_since_activity = 0
+    curr_run_duration += duration
 
 
+def is_tired():
+
+    return time_since_activity < 120
 
 
-
-def get_star_elegible(activity):
+def star_can_be_taken(activity):
     same_activity = (activity == cur_star_activity)
-    correct_time = (cur_time==star_offer_time)
+    correct_time = (cur_time == star_offer_time)
     return same_activity and correct_time and not bored_with_stars
+
 
 def get_cur_hedons():
     return cur_hedons
-    
+
+
 def get_cur_health():
     return cur_health
-    
+
+
 def offer_star(activity):
     global star_offer_time, cur_star_activity
-    print ("get star for: "+ activity)
+    print("get star for: " + activity)
     star_offer_time = cur_time
     cur_star_activity = activity
 
 
 def most_fun_activity_minute():
-    pass
-    
+    global tired
+    if tired:
+        return "resting"
+    # else:
+
 ################################################################################
 # These functions are not required, but we recommend that you use them anyway
 # as helper functions
+
 
 def get_effective_minutes_left_hedons(activity):
     '''Return the number of minutes during which the user will get the full
     amount of hedons for activity activity'''
     pass
-    
+
+
 def get_effective_minutes_left_health(activity):
-    pass    
+    pass
+
 
 def estimate_hedons_delta(activity, duration):
     '''Return the amount of hedons the user would get for performing activity
     activity for duration minutes'''
     pass
-            
+
 
 def estimate_health_delta(activity, duration):
     pass
-        
+
 ################################################################################
-        
+
+
 if __name__ == '__main__':
     initialize()
-    perform_activity("running", 30)    
-    print(get_cur_hedons())            # -20 = 10 * 2 + 20 * (-2)             # Test 1
-    print(get_cur_health())            # 90 = 30 * 3                          # Test 2           		
-    print(most_fun_activity_minute())  # resting                              # Test 3
-    perform_activity("resting", 30)    
-    offer_star("running")              
-    print(most_fun_activity_minute())  # running                              # Test 4
-    perform_activity("textbooks", 30)  
-    print(get_cur_health())            # 150 = 90 + 30*2                      # Test 5
-    print(get_cur_hedons())            # -80 = -20 + 30 * (-2)                # Test 6
+    perform_activity("running", 30)
+    # -20 = 10 * 2 + 20 * (-2)             # Test 1
+    print(get_cur_hedons())
+    # 90 = 30 * 3                          # Test 2
+    print(get_cur_health())
+    # resting                              # Test 3
+    print(most_fun_activity_minute())
+    perform_activity("resting", 30)
+    offer_star("running")
+    # running                              # Test 4
+    print(most_fun_activity_minute())
+    perform_activity("textbooks", 30)
+    # 150 = 90 + 30*2                      # Test 5
+    print(get_cur_health())
+    # -80 = -20 + 30 * (-2)                # Test 6
+    print(get_cur_hedons())
     offer_star("running")
     perform_activity("running", 20)
-    print(get_cur_health())            # 210 = 150 + 20 * 3                   # Test 7
-    print(get_cur_hedons())            # -90 = -80 + 10 * (3-2) + 10 * (-2)   # Test 8
+    # 210 = 150 + 20 * 3                   # Test 7
+    print(get_cur_health())
+    # -90 = -80 + 10 * (3-2) + 10 * (-2)   # Test 8
+    print(get_cur_hedons())
     perform_activity("running", 170)
-    print(get_cur_health())            # 700 = 210 + 160 * 3 + 10 * 1         # Test 9
-    print(get_cur_hedons())            # -430 = -90 + 170 * (-2)              # Test 10
-    
-    
+    # 700 = 210 + 160 * 3 + 10 * 1         # Test 9
+    print(get_cur_health())
+    # -430 = -90 + 170 * (-2)              # Test 10
+    print(get_cur_hedons())
--- test.py
+++ test.py
@@ -1,3 +1,184 @@
-import cv2
+from re import match
+from typing import Match
+# tests
 
-image = cv2.imread("monke.jpg")
+
+def initialize():
+    '''Initializes the global variables needed for the simulation.
+    Note: this function is incomplete, and you may want to modify it'''
+
+    global cur_hedons, cur_health
+
+    global cur_time
+    global last_activity, last_activity_duration
+
+    global last_finished
+    global bored_with_stars
+    global star_offer_time
+    global cur_star_activity
+    global tired
+    global time_since_activity
+    global curr_run_duration
+    time_since_activity = 120
+    cur_hedons = 0
+    cur_health = 0
+    curr_run_duration =0
+    tired = None
+    cur_star = None
+    star_offer_time = None
+    cur_star_activity = None
+
+    bored_with_stars = False
+
+    last_activity = None
+    last_activity_duration = 0
+
+    cur_time = 0
+
+    last_finished = -1000
+
+
+def perform_activity(activity, duration):
+    global cur_hedons, cur_health, tired, cur_time, time_since_activity, last_activity, last_activity_duration, curr_run_duration
+    star_elegible = int(star_can_be_taken(activity))
+    tired = is_tired()
+    if(activity == "running"):
+        curr_run_duration+=duration 
+
+    #     if not tired:  # not tired if not tired or using star
+    #         if duration < 10:
+    #             cur_hedons += 2*10 + (3*star_elegible*10)
+    #             cur_health += 3*duration
+    #         elif duration > 10 and duration < 180:
+    #             cur_hedons += (2)*10+(3*star_elegible *
+    #                                   10) + -2*(duration-10)
+    #             cur_health += 3*duration
+    #         else:
+    #             cur_hedons += (2)*10+(3*star_elegible *
+    #                                   10) + -2*(duration-10)
+    #             cur_health += 3*180 + 1*(duration-180)
+    #     else:
+    #         if(last_activity == "running"):
+
+    #             if duration+last_activity_duration < 10:
+    #                 cur_hedons += (2)*10+(3*star_elegible*10)
+    #                 cur_health += 3*duration
+
+    #             elif duration+last_activity_duration > 10 and duration+last_activity_duration < 180:
+    #                 cur_hedons += (-2)*10+(3*star_elegible *10) + -2*(duration-10)
+    #                 cur_health += 3*duration
+
+    #             else:
+    #                 cur_hedons += (-2)*10+(3*star_elegible *10) + -2*(duration-10)
+    #                 cur_health += 3*180 + 1*(duration-180)
+    #     time_since_activity = 0
+
+    elif(activity == "textbooks"):
+        cur_health += 2*duration
+
+        if not tired:  # not tired if not tired or using star
+            if duration <= 20:
+                # if activity for 10 or under apply star bonus for that time if walked more than 10 only do start bonus for 10
+                cur_hedons += duration + \
+                    (3*duration*star_elegible if duration <
+                     11 else 3*10*star_elegible)
+            else:
+                # star elegible will be either the star bonus or 0
+                cur_hedons += 10 + (duration-10)*-1 + (3*10*star_elegible)
+        if()
+        else: #resting
+            cur_hedons += -2*duration
+        time_since_activity = 0
+    elif(activity == "resting"):
+        time_since_activity +=duration
+    else:
+        return
+
+    cur_time += duration
+
+
+def is_tired():
+    
+    return time_since_activity < 120
+
+
+def star_can_be_taken(activity):
+    same_activity = (activity == cur_star_activity)
+    correct_time = (cur_time == star_offer_time)
+    return same_activity and correct_time and not bored_with_stars
+
+
+def get_cur_hedons():
+    return cur_hedons
+
+
+def get_cur_health():
+    return cur_health
+
+
+def offer_star(activity):
+    global star_offer_time, cur_star_activity
+    print("get star for: " + activity)
+    star_offer_time = cur_time
+    cur_star_activity = activity
+
+
+def most_fun_activity_minute():
+    pass
+
+################################################################################
+# These functions are not required, but we recommend that you use them anyway
+# as helper functions
+
+
+def get_effective_minutes_left_hedons(activity):
+    '''Return the number of minutes during which the user will get the full
+    amount of hedons for activity activity'''
+    pass
+
+
+def get_effective_minutes_left_health(activity):
+    pass
+
+
+def estimate_hedons_delta(activity, duration):
+    '''Return the amount of hedons the user would get for performing activity
+    activity for duration minutes'''
+    pass
+
+
+def estimate_health_delta(activity, duration):
+    pass
+
+################################################################################
+
+
+if __name__ == '__main__':
+    initialize()
+    perform_activity("running", 30)
+    # -20 = 10 * 2 + 20 * (-2)             # Test 1
+    print(get_cur_hedons())
+    # 90 = 30 * 3                          # Test 2
+    print(get_cur_health())
+    # resting                              # Test 3
+    print(most_fun_activity_minute())
+    perform_activity("resting", 30)
+    offer_star("running")
+    # running                              # Test 4
+    print(most_fun_activity_minute())
+    perform_activity("textbooks", 30)
+    # 150 = 90 + 30*2                      # Test 5
+    print(get_cur_health())
+    # -80 = -20 + 30 * (-2)                # Test 6
+    print(get_cur_hedons())
+    offer_star("running")
+    perform_activity("running", 20)
+    # 210 = 150 + 20 * 3                   # Test 7
+    print(get_cur_health())
+    # -90 = -80 + 10 * (3-2) + 10 * (-2)   # Test 8
+    print(get_cur_hedons())
+    perform_activity("running", 170)
+    # 700 = 210 + 160 * 3 + 10 * 1         # Test 9
+    print(get_cur_health())
+    # -430 = -90 + 170 * (-2)              # Test 10
+    print(get_cur_hedons())
